4d7a79da73187b1206e8d5ca7e2d749334408937
Rebecca Lötscher <rebeccaloetscher@hotmail.com>
2017-10-24 15:40:28 +0200

    funct: working version with life countdown still under construction

--- js/app.js
+++ js/app.js
@@ -1,23 +1,24 @@
-// Enemies our player must avoid
+/* General instance of variable Enemy */
 var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
     this.x = x;
     this.y = y;
     this.width = 50;
     this.height = 50;
     this.speed = speed;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+/* Single Enemy start position and speed */
+var enemy1 = new Enemy (0, 60, 150);
+var enemy2 = new Enemy (0, 140, 70);
+var enemy3 = new Enemy (0, 220, 200);
+
+/* List of all available Enemies */
+var allEnemies = [enemy1, enemy2, enemy3];
+
+/* Prototype function to move Enemies;
+   Parameter 'dt': a time delta between ticks*/
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     /* Determines general speed of enemies. */
     this.x += this.speed*dt;
     /* Enemies to re-appear on left side after having
@@ -28,18 +29,12 @@ Enemy.prototype.update = function(dt) {
 
 };
 
-// Draw the enemy on the screen, required method for game
+/* Prototype function to draw Enemies on board */
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-var enemy1 = new Enemy (0, 60, 150);
-var enemy2 = new Enemy (0, 140, 70);
-var enemy3 = new Enemy (0, 220, 90);
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+/* General instance of variable Player */
 var Player = function(x, y) {
   this.x = x;
   this.y = y;
@@ -48,6 +43,12 @@ var Player = function(x, y) {
   this.sprite = "images/char-horn-girl.png";
 };
 
+/* Defines player start position */
+var player = new Player (200, 300);
+
+/* Prototype function to move Player back to start position,
+   either upon reaching the water or having had a collision
+   with an Enemy */
 Player.prototype.update = function() {
   if (this.y < -11) {
     this.x = 200;
@@ -55,10 +56,13 @@ Player.prototype.update = function() {
   }
 };
 
+/* Prototype function to draw Player on board */
 Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+/* Prototype function to move Player on the board by
+   using the arrow keys */
 Player.prototype.handleInput = function (movement) {
   if (movement === "left" && this.x >= 100) {
     this.x -= 100;
@@ -74,25 +78,50 @@ Player.prototype.handleInput = function (movement) {
   }
 };
 
+/* Prototype function to check if collision between
+   Player and Enemy occured */
 Player.prototype.checkCollisions = function () {
-  if (this.x < Enemy.x + Enemy.width &&
-      this.x + this.width > Enemy.x &&
-      this.y < Enemy.y + Enemy.height &&
-      this.height + this.y > Enemy.y) {
+  for(var i = 0 ; i < 3 ; i++)
+  {
+  if ((this.x < allEnemies[i].x + 50) &&
+      (this.x + 50 > allEnemies[i].x) &&
+      (this.y < allEnemies[i].y + 50) &&
+      (50 + this.y > allEnemies[i].y))
+       {
         this.x = 200;
         this.y = 300;
+       }
   }
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-var allEnemies = [enemy1, enemy2, enemy3];
+/* General instance of variable Life */
+var Life = function(x, y) {
+  this.x = x;
+  this.y = y;
+  this.sprite = "images/Heart.png";
+};
 
-// Place the player object in a variable called player
-var player = new Player (200, 300);
+/* Single Life start position */
+var life1 = new Life (20, 500);
+var life2 = new Life (70, 500);
+var life3 = new Life (120, 500);
 
+/* List of all available lifes */
+var allLifes = [life1, life2, life3];
 
+/* Prototype function to show lifes on board */
+Life.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
+/* TODO: Prototype function to remove lifes upon collision */
+Life.prototype.update = function(count) {
+  var count = 3;
+  if (Player.checkCollisions === true) {
+    count -= 1;
+  }
+
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
--- js/engine.js
+++ js/engine.js
@@ -80,6 +80,7 @@ var Engine = (function(global) {
     function update(dt) {
         updateEntities(dt);
         player.checkCollisions();
+    
     }
 
     /* This is called by the update function and loops through all of the
@@ -93,6 +94,9 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
+
+
+
         player.update();
     }
 
@@ -107,12 +111,13 @@ var Engine = (function(global) {
          * for that particular row of the game level.
          */
         var rowImages = [
-                'images/water-block.png',   // Top row is water
+                'images/water-block.png',   // Top Row is water
                 'images/stone-block.png',   // Row 1 of 3 of stone
                 'images/stone-block.png',   // Row 2 of 3 of stone
                 'images/stone-block.png',   // Row 3 of 3 of stone
                 'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/grass-block.png',   // Row 2 of 2 of grass
+
             ],
             numRows = 6,
             numCols = 5,
@@ -150,7 +155,12 @@ var Engine = (function(global) {
             enemy.render();
         });
 
+        allLifes.forEach(function(life) {
+            life.render();
+        });
+
         player.render();
+
     }
 
     /* This function does nothing but it could have been a good place to
@@ -170,7 +180,8 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-horn-girl.png'
+        'images/char-horn-girl.png',
+        'images/Heart.png'
     ]);
 
     Resources.onReady(init);
